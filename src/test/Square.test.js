7b128dd87c5277412a91adc2abc3277085719457
21Neil <88711171+21Neil@users.noreply.github.com>
2024-11-24 21:05:43 +0800

    test: add Square test

--- src/test/Square.test.js
+++ src/test/Square.test.js
@@ -1,4 +1,5 @@
 import Square from "../js/Square";
+import Ship from "../js/Ship";
 
 describe('test square', () => {
   const testSquare = Square()
@@ -7,5 +8,15 @@ describe('test square', () => {
   test('test received attack', () => {
     testSquare.receiveAtk();
     expect(testSquare.isReceivedAtk()).toBeTruthy();
+  })
+  test('test received attack with ship', () => {
+    const ship = Ship(2)
+    testSquareWithShip1.placeShip(ship);
+    testSquareWithShip2.placeShip(ship);
+    testSquareWithShip1.receiveAtk();
+    testSquareWithShip2.receiveAtk();
+    expect(testSquareWithShip1.isReceivedAtk()).toBeTruthy();
+    expect(testSquareWithShip2.isReceivedAtk()).toBeTruthy();
+    expect(ship.isSunk()).toBeTruthy()
   })
 })
