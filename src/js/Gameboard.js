daace7923d82123eabdb631a89b304e86784e276
21Neil <88711171+21Neil@users.noreply.github.com>
2024-11-25 15:07:22 +0800

    feat: check ship overlapping when place ship

--- src/js/Gameboard.js
+++ src/js/Gameboard.js
@@ -14,7 +14,7 @@ const Gameboard = () => {
   };
 
   const isValidPlace = (x, y, direction, length) => {
-    if (!checkValid(x) || !checkValid(y)) return false;
+    if (!checkValid(x) || !checkValid(y)) throw new Error('Coordinates not valid!');
     let shipEnd = 0;
     if (direction === 'v') shipEnd = x + length;
     if (direction === 'h') shipEnd = y + length;
@@ -23,15 +23,41 @@ const Gameboard = () => {
       return num > -1 && num < 10;
     };
 
-    return checkValid(shipEnd);
+    if(!checkValid(shipEnd)) throw new Error('Ship out of gameboard!');
+
+    const checkHadShip = () => {
+      const start = xyToNum(x, y);
+      if(direction === 'v') {
+        const end = start + length - 1
+        for (let i = start; i <= end; i++) {
+          if(board[i].ship !== null) return true
+        }
+      }
+      if(direction === 'h') {
+        const end = start + (length - 1) * 10
+        for (let i = start; i <= end; i += 10) {
+          if(board[i].ship !== null) return true
+        }
+      }
+    }
+
+    if(checkHadShip()) throw new Error("Ship can't overlapping")
   };
 
   const placeShip = (ship, x, y, direction) => {
-    if (!isValidPlace(x, y, direction, ship.length)) return;
+    isValidPlace(x, y, direction, ship.length)
     const start = xyToNum(x, y);
-    const end = start + ship.length;
-    for (let i = start; i < end; direction === 'v' ? i++ : (i += 10)) {
-      board[i].placeShip(ship);
+    if(direction === 'v') {
+      const end = start + length - 1
+      for (let i = start; i <= end; i++) {
+        board[i].placeShip(ship);
+      }
+    }
+    if(direction === 'h') {
+      const end = start + (length - 1) * 10
+      for (let i = start; i <= end; i += 10) {
+        board[i].placeShip(ship);
+      }
     }
   };
 
@@ -59,7 +85,7 @@ const Gameboard = () => {
     const index = xyToNum(x, y);
     if (board[index].isReceivedAtk()) return;
     board[index].receiveAttack();
-    if(checkAllShipSunk()) return 'all ship sank!'
+    if(checkAllShipSunk()) return 'All ship sank!'
   };
 
   const checkAllShipSunk = () => {
